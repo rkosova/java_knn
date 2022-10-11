d4ae206a83f8d925f410b5b0966c91ef39c7f639
Ron Kosova <ron.kosova@gmail.com>
2022-10-11 16:51:30 +0300

    Added constructor for unclassified data points

--- src/main/com/github/rkosova/java_knn/ClassPoint.java
+++ src/main/com/github/rkosova/java_knn/ClassPoint.java
@@ -1,10 +1,17 @@
 package com.github.rkosova.java_knn;
 
+import java.util.Arrays;
+
 public class ClassPoint extends DataPoint {
     private String classification;
 
-    public ClassPoint(String data[], int classColumn) {
+    public ClassPoint(String data[]) { // called when initializing unclassified ClassPoints
         super(data);
+        setClassification(null);
+    }
+
+    public ClassPoint(String data[], int classColumn) { // called when initializing classified ClassPoints
+        super(Arrays.copyOfRange(data, 0, data.length - 1));
         setClassification(data[classColumn - 1]);
     }
 
